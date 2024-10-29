c1030030e1b35f95fedeb48244a48ce50a1b5409
AlexMonzdev <ialexmonzon@gmail.com>
2024-10-29 15:47:40 +0100

    add do/while and exit condition

--- src/main/java/com/alexmonzon/Main.java
+++ src/main/java/com/alexmonzon/Main.java
@@ -2,44 +2,44 @@ package com.alexmonzon;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int option;
         Scanner sc = new Scanner(System.in);
 
+        do {
 
-        System.out.println("1. Añadir Libro");
-        System.out.println("2. Ver todos los libros");
-        System.out.println("3. Eliminar libro");
-        System.out.println("4. Cambiar Repositorio");
-        System.out.println("5. Salir");
+            System.out.println("1. Añadir Libro");
+            System.out.println("2. Ver todos los libros");
+            System.out.println("3. Eliminar libro");
+            System.out.println("4. Cambiar Repositorio");
+            System.out.println("5. Salir");
 
-        System.out.println("Select the option: ");
-        int option = sc.nextInt();
+            System.out.println("Select the option: ");
+            option = sc.nextInt();
 
-        switch (option) {
-            case 1:
-                System.out.println("Option 1: Añadir Libro");
-                break;
-            case 2:
-                System.out.println("Option 2: Ver todos los libros");
-                break;
-            case 3:
-                System.out.println("Option 3: Eliminar libro");
-                break;
-            case 4:
-                System.out.println("Option 4: Cambiar Repositorio");
-                break;
-            case 5:
-                System.out.println("Option 5: Salir");
-                break;
-            default:
-                System.out.println("Opcion no valida");
-                break;
-        }
-
+            switch (option) {
+                case 1:
+                    System.out.println("Option 1: Añadir Libro");
+                    break;
+                case 2:
+                    System.out.println("Option 2: Ver todos los libros");
+                    break;
+                case 3:
+                    System.out.println("Option 3: Eliminar libro");
+                    break;
+                case 4:
+                    System.out.println("Option 4: Cambiar Repositorio");
+                    break;
+                case 5:
+                    System.out.println("Option 5: Salir");
+                    option = 5;
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }while(option != 5);
 
     }
 }
